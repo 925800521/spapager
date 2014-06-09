40dc14d5b0d137e3c1fec0b0af37bd1f397308a4
mennobb <mbieringa@gmail.com>
2014-06-10 00:07:59 +0200

    Release worthy version

--- README.md
+++ README.md
@@ -3,8 +3,63 @@ spapager
 
 SPA Pager is a JavaScript library that provides a simple imitation of jQuery Mobile's "page" concept.
 It is meant for Single Page Applications written in JavaScript and works on mobile devices.
+Both CSS Animation and jQuery's animation function are used in order to support a wide range of devices / browsers
 
-This library has been purposefully kept as lightweight as possible so it can be used in any (mobile) web application without making the project more bulgy.
+This library has been purposefully kept as lightweight as possible so it can be used in any (mobile) web application without making the project much bulgier.
 It is based on jQuery so it would be advisable only to use this library if you’re using jQuery already.
-Also, support for CSS3 animations is assumed. REALLY??
 
+
+Usage
+=====
+
+1. Add pages to your html as follows:
+<div data-role="page" id="pageOne">
+	Hello World
+</div>
+
+2. When the dom is loaded, init the library.
+* All divs with data-role="page" are considered pages.
+* All divs are auto-hidden, except for the first one in the document.
+* All divs get the page class which makes them 100%x100% in size and positioned relatively.
+
+*Short*
+$(function() {
+	spapager.init();
+});
+
+*Long*
+$(function() {
+	spapager.init({
+		cssPrefix:'spapager', // Default: spapager. Change to something else if the CSS classes collide with your css classes. Don't forget to update the CSS classnames accordingly.
+		noCssAnimation: true, // Avoid using CSS animation. Use this if you suspect the browser has no CSS3 animation support.
+		noCssAnimationSpeed: 250 // When NOT using CSS3 animation, define the animation speed here. Otherwise you can determine this in the keyframe definitions in the CSS.
+	});
+});
+
+3. Trigger page switches 
+*Syntax:*
+spapager.changePage(
+	<target page ID>, 
+	{
+		'transition':'slide-up || slide-down || slide-left || slide-right', 
+		'onTransitionEnd': function(eventData) {
+			console.log(eventData);
+		}
+	}
+);
+
+*Examples*
+// No transition. Immediate change.
+spapager.changePage('pageTwo');
+
+// slide up transition.
+spapager.changePage('pageTwo', {'transition':'slide-up'});
+
+// slide up transition and callback once animation ends.
+spapager.changePage('pageTwo', {'transition':'slide-up', onTransitionEnd: function(x) { alert('This is the new page!!!'); }});
+
+4. Add pages
+If you need to add a new page, make sure to use the library's addPage method. 
+Otherwise you will not be able to switch to the new page as the library will not know about it. This function also ass the appropriate classes to the new page.
+*Syntax*
+	var myPage = addPage(<page ID>); // MyPage will hold a jQuery object containing the new page which has been added to the DOM already.
--- spapager.js
+++ spapager.js
@@ -2,29 +2,38 @@ var spapager = (function(){
 	var currentPage; // Holds the currently visible page.
 	var allPageIds; // Holds the ids of all pages. Used for duplicate checking.
 	var cssPrefix = 'spapager'; // Holds the css name for all spapage related css selectors.
-	
+	var noCssAnimation = false; // Set to true if jQuery animation should be used rather than CSS animation
+	var noCssAnimationSpeed = 500; // Set the animations speed when using noCSSAnimation only
+
 	/**
 	 * Initializes the paging concept. 
-	 * Typically executed after the onLoad event triggered. 
+	 * Typically executed after the onLoad event triggered.
 	 * All divs with data-role="page" are considered pages.
-	 * All divs are auto-hidden, except for the first one in the document
-	 * All divs get the page class which makes them 100%x100% in size and positioned relatively
-	 * @param config Optional object containing configuration options for the class
+	 * All divs are auto-hidden, except for the first one in the document.
+	 * All divs get the page class which makes them 100%x100% in size and positioned relatively.
+	 * @param config Optional object containing configuration options for the class.
 	 * Keys:
-	 * cssPrefix: Overrides the standard css prefix "spapager". 
+	 * cssPrefix: Overrides the standard css prefix "spapager".
+	 * noCssAnimation: Set to true to avoid using css3 animations and fall back to jQuery's animate function.
 	 */
 	var init = function(config) {
-		if (typeof config == 'object' && config.cssPrefix) {
-			cssPrefix = config.cssPrefix;
+		if (typeof config == 'object') {
+			if (config.cssPrefix)
+				cssPrefix = config.cssPrefix;
+			
+			if (config.noCssAnimation) {
+				noCssAnimation = true;
+				
+				if (config.noCssAnimationSpeed) {
+					noCssAnimationSpeed = config.noCssAnimationSpeed;
+				}
+			}
 		}
 		
 		$('div[data-role="page"]').addClass('hidden').addClass(cssPrefix);
 		$('div[data-role="page"]:eq(0)').removeClass('hidden').addClass(cssPrefix+'-current');
 		currentPage = $('div[data-role="page"]:eq(0)');
-		
-		
-		
-	}
+	};
 	
 	/**
 	 * Add a new page to the DOM
@@ -53,7 +62,7 @@ var spapager = (function(){
 		
 			//Return a jQuery object referring the new page.
 		return newPage;
-	}
+	};
 	
 	var removePage = function(id) {
 			// If the allPageIds array is not populated yet, do so now.
@@ -74,7 +83,7 @@ var spapager = (function(){
 		}
 		
 		return;
-	}
+	};
 	
 	/**
 	 * Switch from the currently visible page to the page with id <toPage>.  
@@ -106,8 +115,12 @@ var spapager = (function(){
 				onTransitionEnd: false
 			};
 		
-		_changePage(toPage, config);
-	}
+			// Should we use CSS animation?
+		if (noCssAnimation)
+			_changePageNoCSS(toPage, config);
+		else
+			_changePage(toPage, config);
+	};
 	
 	var _changePage = function(toPage, config) {
 		var handleSlide = function(cssAnimationName) {
@@ -123,6 +136,7 @@ var spapager = (function(){
 			);
 		}
 		_startChangePage(toPage);
+		
 		switch(config.transition) {
 			case 'slide-left':
 				handleSlide(cssPrefix+'-slide-left');
@@ -139,7 +153,36 @@ var spapager = (function(){
 			default:
 				_terminateChangePage(toPage, config);
 		}
-	}
+	};
+	
+	var _changePageNoCSS = function(toPage, config) {
+		var handleSlide = function(cssAttr, value) {
+			toPage.css(cssAttr, value);
+			_startChangePage(toPage);
+			var o = {};
+			o[cssAttr]='0%';
+			toPage.animate(o, noCssAnimationSpeed, 'swing',	function() {
+				_terminateChangePage(toPage, config);
+			});
+		}
+		
+		switch(config.transition) {
+			case 'slide-left':
+				handleSlide('left','100%');
+			break;
+			case 'slide-right':
+				handleSlide('left','-100%');
+			break;
+			case 'slide-up':
+				handleSlide('top','100%');
+			break;
+			case 'slide-down':
+				handleSlide('top','-100%');
+			break;
+			default:
+				_terminateChangePage(toPage, config);
+		}
+	};
 	
 	var _findAllPageIds = function() {
 		allPageIds = [];
@@ -148,13 +191,13 @@ var spapager = (function(){
 			if (foundId) allPageIds.push(foundId);
 		});
 		return;
-	}
+	};
 	
 	var _startChangePage = function(toPage) {
 		toPage.addClass(cssPrefix+'-to'); // 
 		currentPage.addClass(cssPrefix+'-current'); // Should already be the case...
 		toPage.removeClass('hidden');
-	}
+	};
 	
 	var _terminateChangePage = function(toPage, config) {
 		currentPage.addClass('hidden').removeClass(cssPrefix+'-current');
@@ -169,7 +212,7 @@ var spapager = (function(){
 				transition: config.transition || ''
 			});
 		}
-	}
+	};
 
 	
 	return {
